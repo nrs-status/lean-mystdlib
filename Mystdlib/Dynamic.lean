544bc622db3ade9d94770b9af62338b052e71bcd
nrs-status <sebaarsim@gmail.com>
2026-04-04 19:32:52 +0000

    added Dynamic.get!

--- Mystdlib/Dynamic.lean
+++ Mystdlib/Dynamic.lean
@@ -9,3 +9,7 @@ deriving instance TypeName for Lean.Expr
 instance : Inhabited Dynamic where
   default := Dynamic.mk Nat.zero
 
+def Dynamic.get! (α : Type u) [Inhabited α] [TypeName α] (v : Dynamic) : α :=
+  match v.get? α with
+  | .some x => x
+  | .none => panic! "Dynamic.get! failed"
