c474bd581c416687ed691ee081dda62b1a9d3038
nrs-status <sebaarsim@gmail.com>
2026-04-12 15:37:07 +0000

    added Optics.lean import file

---
+++ Mystdlib/Optics.lean
@@ -0,0 +1,6 @@
+import Mystdlib.Optics.Categories
+import Mystdlib.Optics.CategoriesInstances
+import Mystdlib.Optics.Tambara
+import Mystdlib.Optics.Optics
+import Mystdlib.Optics.Combinators
+import Mystdlib.Optics.Uniplate
