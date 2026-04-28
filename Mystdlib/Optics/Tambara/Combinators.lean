57333ef2148b5f8f45ad34159550642dd07c9b5e
nrs-status <sebaarsim@gmail.com>
2026-04-28 21:35:40 +0000

    added Tambs instance for composition with Iso, tambara module for App Traversable on (. -> .); some aesthetic changes; Tamb.Prism.elim which behaves like a casesOn for Prism, using ; added Foldable instances; added Aggregating. NOTE: messed up some commit messages in the last 7 commits (or so, those done today). they might lack some information because I went quickly

--- Mystdlib/Optics/Tambara/Combinators.lean
+++ Mystdlib/Optics/Tambara/Combinators.lean
@@ -6,6 +6,12 @@ import Mathlib.Control.Fold
 
 open Tamb
 
+instance [inst : Tambs l p]  : Tambs (l ++ []) p where
+  tambs := by
+    observe : l = l ++ []
+    rw [<- this]
+    exact inst.tambs
+
 instance : Profunctor (· -> ·) where
   map := fun f g h => (g ∘ h) ∘ f
 
@@ -14,6 +20,11 @@ instance : Tamb ⟨Prod.{u,u}, Prod⟩ (· -> ·) where
 
 instance : Tamb ⟨Sum.{u,u}, Sum⟩ (· -> ·) where
   tamb := fun f => Sum.elim .inl (.inr ∘ f)
+
+instance : Tamb ⟨App Traversable, App Traversable⟩ (· -> ·) where
+  tamb := fun {xμ _ _} f => xμ.snd.toFunctor.map f
+
+
 
 instance {α : Type _} : Profunctor (fun (x _ : Type _) => x -> α) where
   map := fun f _ h => h ∘ f
@@ -25,7 +36,11 @@ instance {α : Type _} : Tamb ⟨Prod.{u, u + 1}, Prod⟩ (fun x _ => x -> α) w
   tamb := fun f => fun (_, snd) => f snd
 
 instance {α : Type _} [Mul α] [One α] : Tamb ⟨App Traversable, App Traversable⟩ (fun x _ => x -> α) where
-  tamb := fun {ξ _ xμ} f x => @Traversable.foldMap (t := xμ.fst) xμ.snd ξ α _ _ f x
+  tamb := fun {xμ _ _} f x => have := xμ.snd; Traversable.foldMap f x
+
+open Foldable in
+instance {α : Type u} [Mul α] [One α] : Tamb ⟨App Foldable, ax⟩ (fun x _ => x -> α) where
+  tamb := fun {xμ _ _} f x => xμ.snd.foldMap f x
 
 def view 
   (x : ProfOptic l α β ς τ)
@@ -44,7 +59,7 @@ def review
   [Tambs l (fun _ x => x)]
   : β -> τ
   := x (fun _ x => x)
-  
+
 instance : Profunctor (fun x _ => x -> Option α) where
   map := fun f _ h => h ∘ f
 
@@ -57,8 +72,12 @@ instance : Tamb ⟨Prod.{u,u}, Prod⟩ (fun x _ => x -> Option α) where
 instance : Tamb ⟨Affine, Affine⟩ (fun x _ => x -> Option α) where
   tamb := fun f x => x.elim (fun _ => .none) (f ∘ Prod.snd)
 
+/-
+Tamb.tamb {α β} {xμ : μ} : p α β -> p (axₗ α) (axᵣ β)
+-/
+
 instance : Tamb ⟨App Traversable, App Traversable⟩ (fun x _ => x -> Option α) where
-  tamb := fun {_ _ xμ} f x =>
+  tamb := fun {xμ _ _} f x =>
   have := xμ.snd
   -- Monad.join $ this (fun x y => x.elim (f y) (Option.some ∘ Option.some)) .none x -- previous version
   --Monad.join $ this (fun x y => f y |>.elim (.some x) (Option.some ∘ Option.some)) .none x -- reversed direction
@@ -104,7 +123,7 @@ instance : Tamb ⟨Sum.{u,u}, Sum⟩ (Replacing α β) where
   tamb := fun u f x => x.casesOn Sum.inl (Sum.inr ∘ u f)
 
 instance : Tamb ⟨App Traversable, App Traversable⟩ (Replacing α β) where
-  tamb := fun {_ _ xμ} f g => xμ.snd.toFunctor.map (f g)
+  tamb := fun {xμ _ _} f g => xμ.snd.toFunctor.map (f g)
 
 instance : Tamb ⟨Affine, Affine⟩ (Replacing α β) where
   tamb := fun f g => Sum.elim .inl (fun x => .inr (x.fst, f g x.snd))
@@ -114,7 +133,6 @@ def over
   [Tambs l (Replacing α β)]
   : (α -> β) -> ς -> τ
   := x (Replacing α β) id
-
 
 instance : Profunctor (fun s t => s -> t ⊕ α) where
   map := fun f g h => (Sum.elim (.inl ∘ g) .inr ∘ h) ∘ f
@@ -135,13 +153,67 @@ def matching
   : ς -> τ ⊕ α
   := x (fun s t => s -> t ⊕ α) .inr
 
+def Tamb.Prism.elim
+  /- (x : ProfOptic l α β ς τ) -/
+  /- [Tambs l (fun (s t : Type u) => s -> t ⊕ α)] -/
+  (x : Prism α β ς τ)
+  (f : τ -> γ)
+  (g : α -> γ)
+  : ς -> γ
+  := fun s => match matching x s with
+  | .inl x' => f x'
+  | .inr x' => g x'
+
 -- traversal downcast
 
 instance {p : Type u -> Type _ -> Type _} [inst : Tamb ⟨App Traversable, App Traversable⟩ p] : Tamb ⟨Sum, Sum⟩ p where
-  tamb := fun {α β μ} => @inst.tamb α β ⟨fun x => μ ⊕ x, inferInstance⟩
+  tamb := fun {μ α β} => @inst.tamb ⟨fun x => μ ⊕ x, inferInstance⟩ α β 
 
 instance {p : Type u -> Type _ -> Type _} [inst : Tamb ⟨App Traversable, App Traversable⟩ p] : Tamb ⟨Prod, Prod⟩ p where
-  tamb := fun {α β μ} => @inst.tamb α β ⟨fun x => μ × x, inferInstance⟩
+  tamb := fun {μ α β} => @inst.tamb ⟨fun x => μ × x, inferInstance⟩ α β 
+
+--
+
+section 
+
+open Foldable 
+
+-- m added to def lhs otherwise we get a universe bump
+set_option linter.unusedVariables false in
+abbrev Folding (m : Type u) [Mul m] [One m] (α β ς τ : Type u) := (α -> m) -> (ς -> m)
+
+
+
+instance {α β : Type u} [Mul m] [One m] : Profunctor (Folding m α β) where
+  map := fun f _ h p => h p ∘ f
+
+instance {α β : Type u} [Mul m] [One m] : Tamb ⟨App Foldable, ax⟩ (Folding m α β) where
+  tamb := fun {xμ _ _} f g => xμ.snd.foldMap (f g)
+
+instance {α β : Type u} [Mul m] [One m] : Tamb ⟨App Traversable, ax⟩ (Folding m α β) where
+  tamb := fun {xμ _ _} f g x =>
+    have := @xμ.snd
+    Traversable.foldMap id (Functor.map (f g) x)
+
+instance {α β : Type u} [Mul m] [One m]: Tamb ⟨Prod, Prod⟩ (Folding m α β) where
+  tamb := fun f g x => f g x.snd
+
+def toListOf
+  {α β ς τ : Type u}
+  (x : ProfOptic l α β ς τ)
+  [Tambs l (Folding (List α) α β)]
+  : ς -> List α
+  := x (Folding (List α) α β) id pure
+
+end
+
+def Aggregating (α β ς τ : Type u) := List ς -> (List α -> β) -> τ
+
+instance : Profunctor (Aggregating α β) where
+  map := fun f g h l p => g (h (l.map f) p)
+
+instance : Tamb (μ := Σα, Algebra List α) ⟨algProdAction List, algProdAction List⟩ (Aggregating α β) where
+  tamb := fun {xμ _ _} f l x => have ⟨fst, snd⟩ := l.unzip; (xμ.snd.alg fst, f snd x)
 
 
 
