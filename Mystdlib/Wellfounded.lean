14b38d4aae1a98132615ce852ffbd7d3d32e80b2
nrs-status <sebaarsim@gmail.com>
2026-07-08 14:44:20 +0000

    Acc definition as an iff theorem, and a wellfounded relation 'closer to bound than'

--- Mystdlib/Wellfounded.lean
+++ Mystdlib/Wellfounded.lean
@@ -1,3 +1,5 @@
+import Mathlib.Data.Nat.Lattice
+import Mathlib.Order.WellFounded
 
 theorem acc_iff_forall_rel_acc 
   {rel : α -> α -> Prop}
@@ -8,4 +10,34 @@ theorem acc_iff_forall_rel_acc
       cases h; grind
     · intro h
       constructor; grind
+
+@[grind]
+def CloserToBoundThan (bound a b : Nat) : Prop :=
+  a ≤ bound ∧ bound - a < bound - b
+
+instance : DecidableRel (CloserToBoundThan bound) := by
+  unfold DecidableRel CloserToBoundThan
+  infer_instance
+      
+theorem CloserToBoundThan_wellfounded
+  : WellFounded (CloserToBoundThan bound) := by
+    constructor
+    intro a
+    if a < bound
+    then
+      rw [(by grind : a = bound - (bound - a))]
+      induction bound - a <;> grind [Acc]
+    else grind [Acc]
+
+
+
+
+
+
+
+
+
+
+
+
 
